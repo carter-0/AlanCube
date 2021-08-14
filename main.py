fbf96d42336209f88880770d3239490df967e3b5
Carter <39602818+carter-0@users.noreply.github.com>
2021-08-14 18:56:56 +0000

    remove tingsnio

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ app = Flask(__name__, static_url_path='')
 def random_string_generator(str_size, allowed_chars):
     return ''.join(random.choice(allowed_chars) for x in range(str_size))
 
-@app.route('/scramble')
-def scramble():
-    scramble = ["R ","R' ","L ","L' ","U ","U' ","B ","B' ","D ","D' ","F ","F' "]
-    scramble = random_string_generator(20, scramble)
-
-    return jsonify(scramble=scramble)
-
 @app.route('/')
 def root():
     scramble = ["R ","R' ","L ","L' ","U ","U' ","B ","B' ","D ","D' ","F ","F' "]
