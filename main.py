ae8a5c98fcf599e85658461c15d4355866976f1e
Carter-O <carterannandale@gmail.com>
2021-08-14 19:50:33 +0100

    add alan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, render_template, session, send_from_directory, jsonify, url_for
-import string
 import random
 
 app = Flask(__name__, static_url_path='')
@@ -7,9 +6,16 @@ app = Flask(__name__, static_url_path='')
 def random_string_generator(str_size, allowed_chars):
     return ''.join(random.choice(allowed_chars) for x in range(str_size))
 
+@app.route('/scramble')
+def scramble():
+    scramble = ["R ","R' ","L ","L' ","U ","U' ","B ","B' ","D ","D' ","F ","F' "]
+    scramble = random_string_generator(20, scramble)
+
+    return jsonify(scramble=scramble)
+
 @app.route('/')
 def root():
-    scramble = ["R","R'","L","L'","U","U'","B","B'","D","D'","F","F'"]
+    scramble = ["R ","R' ","L ","L' ","U ","U' ","B ","B' ","D ","D' ","F ","F' "]
     scramble = random_string_generator(20, scramble)
 
     return render_template("index.html", scramble=scramble)
